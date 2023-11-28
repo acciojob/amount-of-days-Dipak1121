e6802897b2d7f3508048480aff1e504d564e3c0f
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-28 16:45:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function daysOfYear(year){
+	if(isYearLeap(year)){
+		return 366;
+	}
+	return 365;
+}
+
+function isYearLeap(year) {
+	if(((year%4 == 0) && (year%100 != 0) ) || (year%400 == 0)){
+		return true;
+	}
+}
